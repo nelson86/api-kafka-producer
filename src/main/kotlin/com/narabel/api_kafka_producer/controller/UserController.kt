bc5cdf17ccaa85f162aa0d0071111ac5a6f49ce9
Nelson Arabel <nelson.arabel@gmail.com>
2022-10-20 11:30:13 -0300

    Added key on event and fun with callbacks

--- src/main/kotlin/com/narabel/api_kafka_producer/controller/UserController.kt
+++ src/main/kotlin/com/narabel/api_kafka_producer/controller/UserController.kt
@@ -3,7 +3,6 @@ package com.narabel.api_kafka_producer.controller
 import com.narabel.api_kafka_producer.publisher.UserPublisher
 import com.narabel.api_kafka_producer.user.UserFactory
 import org.slf4j.LoggerFactory
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,14 +18,14 @@ class UserController(
     private val log = LoggerFactory.getLogger(javaClass)
 
     @GetMapping("/migrate/{size}")
-    fun show(@PathVariable size: Int): ResponseEntity.BodyBuilder {
+    fun show(@PathVariable size: Int) {
 
         repeat(size) {
+            val key = if (it % 2 == 0) "key_even" else "key_odd"
             userPublisher.send(
-                userFactory.createUser()
+                key, userFactory.createUser()
             )
         }
-
-        return ResponseEntity.ok()
+        log.info("Se han enviado $size eventos de usuario creado")
     }
 }
--- src/main/kotlin/com/narabel/api_kafka_producer/publisher/UserPublisher.kt
+++ src/main/kotlin/com/narabel/api_kafka_producer/publisher/UserPublisher.kt
@@ -3,8 +3,12 @@ package com.narabel.api_kafka_producer.publisher
 import com.narabel.api_kafka_producer.dto.KafkaMessage
 import com.narabel.api_kafka_producer.user.User
 import org.slf4j.LoggerFactory
+import org.springframework.kafka.core.KafkaProducerException
+import org.springframework.kafka.core.KafkaSendCallback
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Component
+import org.springframework.util.concurrent.ListenableFuture
 
 @Component
 class UserPublisher(
@@ -14,8 +18,33 @@ class UserPublisher(
     private val log = LoggerFactory.getLogger(javaClass)
     private val topic: String = "users.v1.created"
 
-    fun send(user: User) {
-        val response = producer.send(topic, KafkaMessage("created", user))
+    fun send(key: String, user: User) {
+        // asynchronous send: faster
+        producer.send(topic, key, KafkaMessage("created", user))
+
+        // synchronous send
+        // producer.send(topic, key, KafkaMessage("created", user)).get()
+
         log.trace(user.toString())
+    }
+
+    fun sendWithCallbacks(key: String, user: User) {
+
+        val future: ListenableFuture<SendResult<String, KafkaMessage>> = producer.send(topic, key, KafkaMessage("created", user))
+
+        future.addCallback(object : KafkaSendCallback<String, KafkaMessage> {
+
+            override fun onSuccess(result: SendResult<String, KafkaMessage>?) {
+                log.info("Message sent")
+            }
+
+            override fun onFailure(ex: Throwable) {
+                log.error("Error sending message ", ex)
+            }
+
+            override fun onFailure(ex: KafkaProducerException) {
+                log.error("Error sending message ", ex)
+            }
+        })
     }
 }
